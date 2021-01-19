6252383ac8d5cbcf8bff51c486d36d683b8384db
Pedro Pires <pcaetanop@gmail.com>
2021-01-19 11:02:48 -0300

    Adding background wrapper

--- src/ItemContainer.js
+++ src/ItemContainer.js
@@ -3,16 +3,20 @@ import Item from "./Item.js";
 
 function ItemContainer(props) {
   return (
-    <div className="itemContainer">
-      {props.item.map((item) => {
-        return (
-          <>
-            <div key={item.id.toString()}>
-              <Item className="item" img={item.full_background} />
-            </div>
-          </>
-        );
-      })}
+    <div className="wrapper">
+      <div className="background">
+        <div className="itemContainer">
+          {props.item.map((item) => {
+            return (
+              <>
+                <div key={item.id.toString()}>
+                  <Item className="item" img={item.full_background} />
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 }
